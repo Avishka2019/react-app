89240042c2af836eabc5173b1a0250c6e3d9f23a
Avishka2019 <avishkadevinda2017@gmail.com>
2020-10-26 16:56:41 +0530

    Updates

---
+++ precache-manifest.5fdcb39c880e27cf50620df10e617a00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "967bfab33e35862e73ae",
+    "url": "/react-app/static/css/main.75038e56.chunk.css"
+  },
+  {
+    "revision": "967bfab33e35862e73ae",
+    "url": "/react-app/static/js/main.967bfab3.chunk.js"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "d38417b92ecd31c61eac",
+    "url": "/react-app/static/js/2.d38417b9.chunk.js"
+  },
+  {
+    "revision": "526b74804a44f876fa02417b61ac32d6",
+    "url": "/react-app/index.html"
+  }
+];
